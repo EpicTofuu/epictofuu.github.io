4935677e169377c60a59ec5367a4c5947872a4f1
SheiTofu <rinkaandchan@outlook.com>
2017-08-10 16:48:25 +1000

    idk

--- javascript/index.js
+++ javascript/index.js
@@ -1,23 +1,9 @@
 window.onload = function() {
-	games();
 	show();
 }
 
 /*I know I can just parse in an int for one method and then call said method with int inside parenthesis but this is javascript and javascript is scary asf
 did some shit in mobile.js though 8)*/
-function games ()
-{
-	document.getElementById("games").style.visibility="visible";
-	document.getElementById("music").style.visibility="hidden";
-	document.getElementById("misc").style.visibility="hidden";
-}
-
-function misc ()
-{
-	document.getElementById("games").style.visibility="hidden";
-	document.getElementById("music").style.visibility="hidden";
-	document.getElementById("misc").style.visibility="visible";
-}
 
 function hide ()
 {
