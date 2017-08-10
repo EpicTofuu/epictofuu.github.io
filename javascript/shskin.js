35f29ede7e01384f0c868a05562e54a6ebb3a40f
SheiTofu <rinkaandchan@outlook.com>
2017-08-10 16:40:37 +1000

    new shit

--- javascript/shskin.js
+++ javascript/shskin.js
@@ -1,24 +1,35 @@
 window.onload = function() {
-	sh17();
+	sh172();
+}
+
+function sh172 ()
+{
+	  document.getElementById("current").style.visibility="visible";
+  	document.getElementById("Sh17").style.visibility="hidden";
+    document.getElementById("Sh").style.visibility="hidden";
+    document.getElementById("ssfc").style.visibility="hidden";
 }
 
 function sh17 ()
 {
-  	document.getElementById("current").style.visibility="visible";
+		document.getElementById("current").style.visibility="hidden";
+  	document.getElementById("Sh17").style.visibility="visible";
     document.getElementById("Sh").style.visibility="hidden";
-    document.getElementById("ShSSFCsucc").style.visibility="hidden";
+    document.getElementById("ssfc").style.visibility="hidden";
 }
 
 function sh ()
 {
-  document.getElementById("current").style.visibility="hidden";
+	document.getElementById("current").style.visibility="hidden";
+  document.getElementById("Sh17").style.visibility="hidden";
   document.getElementById("Sh").style.visibility="visible";
-  document.getElementById("ShSSFCsucc").style.visibility="hidden";
+  document.getElementById("ssfc").style.visibility="hidden";
 }
 
 function ssfc ()
 {
-  document.getElementById("ShSSFCsucc").style.visibility="visible";
-  document.getElementById("current").style.visibility="hidden";
+	document.getElementById("current").style.visibility="hidden";
+  document.getElementById("ssfc").style.visibility="visible";
+  document.getElementById("Sh17").style.visibility="hidden";
   document.getElementById("Sh").style.visibility="hidden";
 }
